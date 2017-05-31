7b6117275b82010f45ba474dd60a2d815c571aa5
Jose Vieira <hello@josevieira.me>
2017-05-31 02:44:13 -0300

    Added Database

--- readme.md
+++ readme.md
@@ -10,11 +10,6 @@ composer require josevieirame/laravel-support
 
 Only PHP 5.6 or above are supported by this version.
 
-## Testing
-``` bash
-vendor/bin/phpunit
-```
-
 ## License
 
 The MIT License (MIT). Please see [License File](license.md) for more information.
---
+++ src/Database/Migration.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Josevieirame\Support\Database;
+
+use Illuminate\Database\Migrations\Migration as LaravelMigration;
+
+/**
+ * Class Migration.
+ */
+abstract class Migration extends LaravelMigration
+{
+    /**
+     * @var \Illuminate\Database\Schema\Builder
+     */
+    protected $schema;
+
+    public function __construct()
+    {
+        $this->schema = app('db')->connection()->getSchemaBuilder();
+    }
+
+    abstract public function up();
+
+    abstract public function down();
+}
